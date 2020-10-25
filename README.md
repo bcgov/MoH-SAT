54f4c990897367af3ca94cf4fc2d3acb22ca1211
Jan Julian <jjulian007@ca.nam.ad.pwcinternal.com>
2020-10-25 16:38:05 -0700

    Initial seed.
    Org setup scripts. Add readme file.
    
    Data model + salesforce app.
    
    Setup scripts. Dev metadata.
    
    Admin permission set.
    
    Fix devhub alias
    
    Dev setup fixes.
    
    Fix batch script
    
    Add person account fields.
    
    UI Tweaks (page layouts and record page layouts)
    
    UI Tweaks (page layouts and record page layouts)
    
    Perm set changes for new sort order fields.
    
    Manifest for retrieving profiles.
    
    Admin profile changes.
    
    Add drug name field.
    
    List views and search layouts
    
    Data seed scripts.
    
    List views.
    
    Change Provider lookup from Account to Contact
    
    Expand field length.
    
    Case data seed.

---
+++ README.md
@@ -0,0 +1,11 @@
+# SAT
+
+1. Run developer setup script.
+```
+$ ./dev-setup.sh
+```
+
+1. Create a scratch org.
+1. Push source code to scratch org.
+1. Assign permission set to user.
+1. Set current user's role.
---
+++ dev-setup.bat
@@ -0,0 +1,7 @@
+call sfdx force:org:create -v devhub -a sat-dev -f config/project-scratch-def.json -d 30 -s
+call sfdx force:source:push
+call sfdx force:user:permset:assign -n SA_Administrator
+call sfdx force:apex:execute -f scripts/apex/set-user-role.apex
+call sfdx force:data:bulk:upsert -s Drug__c -f data\drugs.csv -i API_Name__c -w 5
+call sfdx force:data:bulk:upsert -s Account -f data\accounts.csv -i Id -w 5
+call sfdx force:data:bulk:upsert -s Case -f data\cases.csv -i Id -w 5
