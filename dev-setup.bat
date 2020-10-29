31a4ddf95da726fa61b35c23b7055a4825651f59
Jan Julian <jjulian007@ca.nam.ad.pwcinternal.com>
2020-10-28 19:06:40 -0700

    Seed form responses.

--- dev-setup.bat
+++ dev-setup.bat
@@ -6,4 +6,5 @@ call sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data\drugs.csv -i Id -
 call sfdx force:data:bulk:upsert -u sat-dev -s Account -f data\accounts.csv -i Id -w 5
 call sfdx force:data:bulk:upsert -u sat-dev -s Case -f data\cases.csv -i Id -w 5
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-queue.apex
+call sfdx force:apex:execute -u sat-dev -f scripts/apex/add-form-questions.apex
 call sfdx force:org:open -u sat-dev
