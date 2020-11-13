991b2a496bdc300d77f9f6ad8bdcbfcd416d5c19
Jan Julian <jan@graycloud.ca>
2020-11-12 19:43:51 -0800

    Fix directory slashes.

--- dev-setup.bat
+++ dev-setup.bat
@@ -2,9 +2,9 @@ call sfdx force:org:create -v devhub -a sat-dev -f config/project-scratch-def.js
 call sfdx force:source:push -u sat-dev
 call sfdx force:user:permset:assign -u sat-dev -n SA_Administrator
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/set-user-role.apex
-call sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data\drugs.csv -i Id -w 5
-call sfdx force:data:bulk:upsert -u sat-dev -s Account -f data\accounts.csv -i Id -w 5
-call sfdx force:data:bulk:upsert -u sat-dev -s Case -f data\cases.csv -i Id -w 5
+call sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data/drugs.csv -i Id -w 5
+call sfdx force:data:bulk:upsert -u sat-dev -s Account -f data/accounts.csv -i Id -w 5
+call sfdx force:data:bulk:upsert -u sat-dev -s Case -f data/cases.csv -i Id -w 5
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-queue.apex
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/add-form-questions.apex
 call sfdx force:org:open -u sat-dev
