c947a609dbf927950ae91d77e9962fb1d4adedd2
Mark Lisé <marklise@users.noreply.github.com>
2020-11-17 08:47:46 -0800

    Benefits response class, SA history naming, stubbed out provider information component. (#34)
    * Adding benefits response class, update to SA history naming
    
    * Adding stubbed out ui/data for provider information.

---
+++ force-app/main/default/lwc/providerInformation/providerInformation.js
@@ -0,0 +1,17 @@
+import { LightningElement, api, wire } from 'lwc';
+import verifyCollegeInformation from '@salesforce/apex/ODRIntegration.verifyCollegeInformation';
+
+export default class ProviderInformation extends LightningElement {
+  @api recordId;
+  verified = false;
+  loaded = false;
+  data = null;
+
+  @wire(verifyCollegeInformation, { recordId: '$recordId', collegeNumber: '', collegeName: '' }) mapObjectToData({error,data}) {
+    if (data) {
+      console.log("DATA:", data);
+      this.data = data;
+      this.loaded = true;
+    }
+  }
+}
