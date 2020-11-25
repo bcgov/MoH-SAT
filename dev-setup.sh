53c2534dc72a4c00a363b000b254085c5f76b10e
Jan Julian <jan@graycloud.ca>
2020-11-25 12:27:05 -0800

    Add sh file

---
+++ dev-setup.sh
@@ -0,0 +1,10 @@
+sfdx force:org:create -v devhub -a sat-dev -f config/project-scratch-def.json -d 7 -s;
+sfdx force:source:push -u sat-dev;
+sfdx force:user:permset:assign -u sat-dev -n SA_Administrator;
+sfdx force:apex:execute -u sat-dev -f scripts/apex/set-user-role.apex;
+sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data/drugs.csv -i Id -w 5;
+sfdx force:data:bulk:upsert -u sat-dev -s Account -f data/accounts.csv -i Id -w 5;
+sfdx force:data:bulk:upsert -u sat-dev -s Case -f data/cases.csv -i Id -w 5;
+sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-queue.apex;
+sfdx force:apex:execute -u sat-dev -f scripts/apex/add-form-questions.apex;
+sfdx force:org:open -u sat-dev;
