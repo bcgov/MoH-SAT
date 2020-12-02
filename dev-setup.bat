01b23342e5f5877310303fd780db7e8502258693
Jan Julian <jan@graycloud.ca>
2020-12-02 10:57:31 -0800

    Adjudication tweaks.
    Support drug object on queues.
    
    Adjudication critieria data import
    
    Data setup scripts.
    
    Page layout tweaks.
    
    Rearrange csv
    
    Move list views outside of package
    
    Limit adjudication to a single case.

--- dev-setup.bat
+++ dev-setup.bat
@@ -7,4 +7,7 @@ call sfdx force:data:bulk:upsert -u sat-dev -s Account -f data/accounts.csv -i I
 call sfdx force:data:bulk:upsert -u sat-dev -s Case -f data/cases.csv -i Id -w 5
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-queue.apex
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/add-form-questions.apex
+call sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data/drug-settings.csv -i Id -w 5
+call sfdx force:data:bulk:upsert -u sat-dev -s Adjudication_Criteria__c -f data/adjudicationcriteria.csv -i Id -w 5
+call sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-drug-default-queues.apex
 call sfdx force:org:open -u sat-dev
