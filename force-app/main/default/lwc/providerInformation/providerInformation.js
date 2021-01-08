eb3a0b098fdf2ee410af5065a158eb172f388a8e
Mark Lisé <marklise@users.noreply.github.com>
2021-01-07 17:06:35 -0800

    Adding get practitioner, updating logger / error logging calls. (#126)

--- force-app/main/default/lwc/providerInformation/providerInformation.js
+++ force-app/main/default/lwc/providerInformation/providerInformation.js
@@ -10,7 +10,24 @@ export default class ProviderInformation extends LightningElement {
   @wire(verifyCollegeInformation, { recordId: '$recordId', collegeNumber: '', collegeName: '' }) mapObjectToData({error,data}) {
     if (data) {
       console.log("ProviderInformation:", data);
-      this.data = data;
+      this.data = {};
+      this.data['firstName'] = data.firstName;
+      this.data['middleInitial'] = data.middleInitial;
+      this.data['lastName'] = data.lastName;
+      this.data['dateofBirth'] = data.dateofBirth;
+      this.data['status'] = data.status;
+      this.data['effectiveDate'] = data.effectiveDate;
+      this.data['verified'] = data.verified;
+      this.data['name'] = "";
+      if (data.firstName) {
+        this.data.name += data.firstName;
+      }
+      if (data.middleInitial) {
+        this.data.name += ' ' + data.middleInitial;
+      }
+      if (data.lastName) {
+        this.data.name += ' ' + data.lastName;
+      }
       this.loaded = true;
     }
   }
