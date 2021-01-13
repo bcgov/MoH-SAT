70b8d33908b80a4ef7f4bfde1ae5d4deee08e18b
Mark Lisé <mark@digitalspace.ca>
2021-01-12 18:13:25 -0800

    Adding updated templates, changing locations, added selectable component for generating PDF

---
+++ force-app/main/default/lwc/faxTemplateChooser/faxTemplateChooser.js
@@ -0,0 +1,35 @@
+import { LightningElement, api, track } from 'lwc';
+import getTemplates from '@salesforce/apex/FolderUtility.getTemplates';
+
+export default class FaxTemplateChooser extends LightningElement {
+  @api recordId;
+  isDisabled = true;
+  data = null;
+  value = '';
+  options = [ ];
+
+  @track error;
+  connectedCallback() {
+    getTemplates({recordId: this.recordId})
+    .then(data => {
+      this.options = data.map(item => {
+        return {
+          label: item.Name,
+          value: item.Id
+        }
+      });
+    })
+  }
+
+  handleChange(event) {
+    this.value = event.detail.value;
+    this.isDisabled = false;
+  }
+  get options() {
+    return this.options;
+  }
+
+  generatePDF(event) {
+    window.open('/apex/PDFGenerator?id=' + this.recordId + '&templateId=' + this.value)
+  }
+}
