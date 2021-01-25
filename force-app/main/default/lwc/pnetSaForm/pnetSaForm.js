0eafc8c1ef2b81fb9f2d553029ac9aef2228caa3
Jan Julian <jan@graycloud.ca>
2021-01-25 15:49:06 -0800

    Editable pharmanet payload.

--- force-app/main/default/lwc/pnetSaForm/pnetSaForm.js
+++ force-app/main/default/lwc/pnetSaForm/pnetSaForm.js
@@ -1,6 +1,7 @@
 import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+import postSingleSAApproval from '@salesforce/apex/ODRIntegration.postSingleSAApproval';
 export default class PnetSaForm extends LightningElement {
     @api
     caseId;
@@ -8,7 +9,26 @@ export default class PnetSaForm extends LightningElement {
     _record;
 
     get record() {
-        return this._record;
+        return {
+            saRecord: {
+                phn: this._record.phn,
+                specAuthType: this._record.specAuthType,
+                justificationCodes: this.strToArr(this._record.justificationCodes),
+                excludedPlans: this.strToArr(this._record.excludedPlans),
+                effectiveDate: this.dateSfdcToOdr(this._record.effectiveDate),
+                terminateDate: this.dateSfdcToOdr(this._record.terminateDate),
+                maxDaysSupply: this._record.maxDaysSupply,
+                maxPricePct: this._record.maxPricePct,
+                saRequester: {
+                    practIdRef: this._record.practIdRef,
+                    practId: this._record.practId
+                },
+                specialItem: {
+                    din: this._record.din,
+                    rdp: this._record.rdp
+                }
+            }
+        };
     }
 
     @api
@@ -20,8 +40,8 @@ export default class PnetSaForm extends LightningElement {
             din: value.saRecord.specialItem.din,
             rdp: value.saRecord.specialItem.rdp,
             specAuthType: value.saRecord.specAuthType,
-            justificationCodes: value.saRecord.justificationCodes,
-            excludedPlans: value.saRecord.excludedPlans,
+            justificationCodes: this.arrToStr(value.saRecord.justificationCodes),
+            excludedPlans: this.arrToStr(value.saRecord.excludedPlans),
             effectiveDate: this.dateOdrToSfdc(value.saRecord.effectiveDate),
             terminationDate: this.dateOdrToSfdc(value.saRecord.terminationDate),
             maxDaysSupply: value.saRecord.maxDaysSupply,
@@ -34,11 +54,40 @@ export default class PnetSaForm extends LightningElement {
         return odrDateStr.replaceAll('/', '-');
     }
 
+    dateSfdcToOdr(sfdcDateStr) {
+        if (!sfdcDateStr) return null;
+        return sfdcDateStr.replaceAll('-', '/');
+    }
+
+    arrToStr(arr) {
+        return arr ? arr.join(',') : '';
+    }
+
+    strToArr(strValue) {
+        return strValue.split(',').map(item => item.trim());
+    }
+
     handleFormChange(event) {
         this._record[event.currentTarget.dataset.field] = event.target.value;
     }
 
-    handleSubmit() {
-        console.log(this._record);
+    async handleSubmit() {
+        this.template.querySelector('.btn-submit').disabled = true;
+
+        try {
+            let saResponse = await postSingleSAApproval({
+                caseId: this.caseId,
+                pnetSa: this.record
+            });
+        } catch (error) {
+            this.dispatchEvent(new ShowToastEvent({
+                title: 'Error',
+                message: error.body.message,
+                mode: "sticky",
+                variant: "error"
+            }));
+            this.template.querySelector('.btn-submit').disabled = false;
+        }
+        
     }
 }
