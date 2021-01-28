a19fd48489c3f77af02de4639827e04077370a8f
Jan Julian <jan@graycloud.ca>
2021-01-28 09:59:45 -0800

    Remove flow validation api.

--- force-app/main/default/lwc/odrLookup/odrLookup.js
+++ force-app/main/default/lwc/odrLookup/odrLookup.js
@@ -38,40 +38,4 @@ export default class OdrLookup extends LightningElement {
         }
     }
 
-    @api
-    validate() {
-        const prescriberValid = this.prescriber && this.prescriber.verified;
-        const patientValid = this.patient;
-        const submitterValid = this.submitter === undefined || (this.submitter && this.submitter.verified);
-        
-        const valid = {
-            isValid: false,
-            errorMessage: []
-        };
-
-        if (!prescriberValid) {
-            valid.isValid = false;
-            valid.errorMessage.push('Prescriber is empty or not practicing.');
-        } 
-        if (!patientValid) {
-            valid.isValid = false;
-            valid.errorMessage.push('Patient is empty or invalid.');
-        } 
-        if (!submitterValid) {
-            valid.isValid = false;
-            valid.errorMessage.push('Submitter is not practicing.');
-        } 
-
-        if (prescriberValid && patientValid && submitterValid) {
-            valid.isValid = true;
-            valid.errorMessage = undefined;
-        }
-        
-        if (valid.isValid == false) {
-            valid.errorMessage = valid.errorMessage.join('\n');
-        }
-
-        return valid;
-    }
-
 }
