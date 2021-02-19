8a0ce9c1a9e91f300268b46a0da308dee48c686e
Mark Lisé <marklise@users.noreply.github.com>
2021-02-18 17:59:31 -0800

    Fix dates/bugs (#237)
    Adding patient lookup/adjustments
    Cleanup

--- force-app/main/default/lwc/patientLookup/patientLookup.js
+++ force-app/main/default/lwc/patientLookup/patientLookup.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, api } from 'lwc';
 import { getObjectInfo, getPicklistValues } from 'lightning/uiObjectInfoApi';
+import findPatient from '@salesforce/apex/EmpiLookup.findPatient';
 
 import OBJ_ACCOUNT from '@salesforce/schema/Account';
 import OBJ_CONTACT from '@salesforce/schema/Contact';
@@ -11,11 +12,10 @@ export default class PatientLookup extends LightningElement {
     @api
     iconName = 'utility:search';
 
+    completeAndNoResults = false;
+    hasData = false;
     patient = {};
     odrPatient = {};
-
-    @wire(getObjectInfo, { objectApiName: OBJ_ACCOUNT })
-    accountObjInfo;
 
     @wire(getObjectInfo, { objectApiName: OBJ_CONTACT })
     contactObjInfo;
@@ -33,19 +33,60 @@ export default class PatientLookup extends LightningElement {
     }
 
     get patientRecordTypeId() {
-        return Object.values(this.accountObjInfo.data.recordTypeInfos).find(rti => rti.name=='Patient').recordTypeId;
+        return Object.values(this.contactObjInfo.data.recordTypeInfos).find(rti => rti.name=='Master').recordTypeId;
     }
 
     handleFormChange(event) {
         this.patient[event.currentTarget.dataset.field] = event.target.value;
-        this.publishChange(this.patient);
+
+        this.template.querySelector('.btn-lookup').disabled
+            = !this.patient.Patient_Identifier__pc;
     }
 
-    handleLookup() {
+    async handleLookup() {
+        this.template.querySelector('.btn-lookup').disabled = true;
 
+        this.patient = {
+            RecordTypeId: this.patientRecordTypeId,
+            Patient_Identifier__pc: this.patient.Patient_Identifier__pc
+        };
+
+        this.patientProvider = await findPatient({
+            phn: this.patient.Patient_Identifier__pc,
+        });
+
+        if (this.patientProvider.gender == undefined,
+            this.patientProvider.dob == undefined,
+            this.patientProvider.familyName == undefined,
+            this.patientProvider.givenName == undefined)
+        {
+            // No Data!
+            this.hasData = false;
+            this.completeAndNoResults = true;
+        } else {
+            this.completeAndNoResults = false;
+            this.hasData = true;
+
+            this.patient = {
+                FirstName: this.patientProvider.givenName,
+                LastName: this.patientProvider.familyName,
+                Gender: this.patientProvider.gender,
+                Deceased: this.patientProvider.deceased,
+                PersonBirthdate: new Date(this.patientProvider.dob),
+                ...this.patient
+            }
+        }
+
+        this.publishChange(this.patient);
+
+        this.template.querySelector('.btn-lookup').disabled = false;
     }
 
     publishChange(record) {
         this.dispatchEvent(new CustomEvent('change', { detail: record }));
+    }
+
+    get noRecord() {
+        return this.patientProvider && this.patientProvider.verified === undefined;
     }
 }
