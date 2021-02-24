7588e6b6866848e5b1cebdb89cd1ecbeeeca2bee
Jan Julian <jan@graycloud.ca>
2021-02-23 16:30:32 -0800

    Case layout changes. Scratch org improvements. (#243)
    * Remove workflow meta
    
    * Seed data refresh for accounts.
    
    * Use tab for top related list.
    
    * Record page layout changes.
    
    * Set user fed id. Fix build script.

--- dev-setup.bat
+++ dev-setup.bat
@@ -1,7 +1,7 @@
 call sfdx force:org:create -v devhub -a sat-dev -f config/project-scratch-def.json -d 7 -s
 call sfdx force:source:push -u sat-dev
 call sfdx force:user:permset:assign -u sat-dev -n SA_Administrator
-call sfdx force:apex:execute -u sat-dev -f scripts/apex/set-user-role.apex
+call sfdx force:apex:execute -u sat-dev -f scripts/apex/set-current-user.apex
 call sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data/drugs.csv -i Id -w 5
 call sfdx force:data:bulk:upsert -u sat-dev -s Account -f data/accounts.csv -i Id -w 5
 call sfdx force:data:bulk:upsert -u sat-dev -s Case -f data/cases.csv -i Id -w 5
