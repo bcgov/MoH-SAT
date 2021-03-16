4f7f0e573cee6e712c9300fac1d9709bd9c56b73
Mark Lisé <marklise@users.noreply.github.com>
2021-03-16 14:55:48 -0700

    ESA-942: Adjust column data for SA History (#291)

--- force-app/main/default/lwc/pharmanetApprovalHistory/pharmanetApprovalHistory.js
+++ force-app/main/default/lwc/pharmanetApprovalHistory/pharmanetApprovalHistory.js
@@ -5,10 +5,16 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 const columns = [
   { label: 'Description', fieldName: 'description', type: 'text',  initialWidth: 120, hideDefaultActions: true },
   { label: 'Code', fieldName: 'dinrdp', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Auth Type', fieldName: 'specAuthType', type: 'text', wrapText: true, hideDefaultActions: true },
   { label: 'Effective Date', fieldName: 'effectiveDate', type: 'date-local', typeAttributes:{ month: "2-digit", day: "2-digit" }, hideDefaultActions: true },
   { label: 'Termination Date', fieldName: 'terminationDate', type: 'date-local', typeAttributes:{ month: "2-digit", day: "2-digit" }, hideDefaultActions: true },
+  { label: 'Pract ID', fieldName: 'practId', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Pract ID Ref', fieldName: 'practIdRef', type: 'text', wrapText: true, hideDefaultActions: true },
   { label: 'DaysSupply', fieldName: 'maxDaysSupply', type: 'text', wrapText: true, hideDefaultActions: true },
-  { label: '%', fieldName: 'maxPricePct', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Excluded Plans', fieldName: 'excludedPlans', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Pharmacy', fieldName: 'pharmacyID', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'DEC', fieldName: 'decCode', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'CreatedBy', fieldName: 'createdBy', type: 'text', wrapText: true, hideDefaultActions: true }
 ];
 
 export default class PharmanetApprovalHistory extends LightningElement {
@@ -40,13 +46,27 @@ export default class PharmanetApprovalHistory extends LightningElement {
         let dataArray = [];
         records.forEach(record => {
           let item = {};
-
           item['description'] = record.specialItem.itemDescription;
           item['dinrdp'] = record.specialItem.din || record.specialItem.rdp;
+          item['specAuthType'] = record.specAuthType;
           item['effectiveDate'] = record.effectiveDate;
           item['terminationDate'] = record.terminationDate;
+          item['practId'] = record.saRequester.practId;
+          item['practIdRef'] = record.saRequester.practIdRef;
+
+          item['excludedPlans'] = "";
+          record.excludedPlans.forEach(ep => {
+            if (item['excludedPlans'] == "") {
+              item['excludedPlans'] = ep;
+            } else {
+              item['excludedPlans'] += ", " + ep
+            }
+          });
           item['maxDaysSupply'] = record.maxDaysSupply;
-          item['maxPricePct'] = record.maxPricePct;
+          item['pharmacyID'] = record.saRequester.pharmacyID;
+          // Not coming in response.
+          item['decCode'] = record.saRequester.decCode;
+          item['createdBy'] = record.createdBy;
           dataArray.push(item);
         });
         this.data = dataArray;
