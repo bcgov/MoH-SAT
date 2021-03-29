515190a298d732ce7daae5b350c8d893320a19bc
Jan Julian <jan@graycloud.ca>
2021-03-29 12:07:50 -0700

    Add community data (#323)

--- dev-setup.bat
+++ dev-setup.bat
@@ -5,6 +5,9 @@ call sfdx force:apex:execute -u sat-dev -f scripts/apex/set-current-user.apex
 call sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data/drugs.csv -i Drug_Code__c -w 5
 call sfdx force:data:bulk:upsert -u sat-dev -s Account -f data/accounts.csv -i Id -w 5
 call sfdx force:data:bulk:upsert -u sat-dev -s Case -f data/cases.csv -i Id -w 5
+call sfdx force:data:tree:import -p data/comm-plan.json
+call sfdx force:apex:execute -u sat-dev -f scripts/apex/add-comm-users.apex
+call sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-ecs.apex
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-queue.apex
 call sfdx force:apex:execute -u sat-dev -f scripts/apex/add-form-questions.apex
 call sfdx force:data:bulk:upsert -u sat-dev -s Adjudication_Criteria__c -f data/adjudicationcriteria.csv -i Id -w 5;
