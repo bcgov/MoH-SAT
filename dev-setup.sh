8081e9bafa4cc19905b4bdc587bda582228dce64
Jan Julian <jan@graycloud.ca>
2021-04-30 16:38:01 -0700

    Dev setup script enhancement.
    Add docs.

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,18 +1,73 @@
-sfdx force:org:create -v devhub -a sat-dev -f config/project-scratch-def.json -d 7 -s;
-sfdx force:source:push -u sat-dev;
-sfdx force:user:permset:assign -u sat-dev -n SA_Administrator;
-sfdx force:apex:execute -u sat-dev -f scripts/apex/set-current-user.apex;
-sfdx force:data:bulk:upsert -u sat-dev -s Drug__c -f data/drugs.csv -i Drug_Code__c -w 5;
-sfdx force:data:bulk:upsert -u sat-dev -s Account -f data/accounts.csv -i Id -w 5;
-sfdx force:data:bulk:upsert -u sat-dev -s Case -f data/cases.csv -i Id -w 5;
-sfdx force:data:tree:import -p data/comm-plan.json;
-sfdx force:apex:execute -u sat-dev -f scripts/apex/add-comm-users.apex;
-sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-ecs.apex;
-sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-cases-to-queue.apex;
-sfdx force:apex:execute -u sat-dev -f scripts/apex/add-form-questions.apex;
-sfdx force:data:bulk:upsert -u sat-dev -s Adjudication_Criteria__c -f data/adjudicationcriteria.csv -i Id -w 5;
-sfdx force:data:bulk:upsert -u sat-dev -s Diagnosis__c -f data/diagnosis.csv -i Id -w 5;
-sfdx force:data:bulk:upsert -u sat-dev -s Drug_Template__c -f data/drugtemplates.csv -i Id -w 5;
-sfdx force:data:bulk:upsert -u sat-dev -s Product_Health_Category__c -f data/producthealthcategories.csv -i Id -w 5;
-sfdx force:apex:execute -u sat-dev -f scripts/apex/assign-drug-default-queues.apex;
-sfdx force:org:open -u sat-dev;
+#!/bin/bash
+
+help() {
+   echo ""
+   echo "Usage: $0 -a <string>"
+   echo -e "\t-a Set an alias for scratch org. Default 'sat-dev'."
+   echo -e "\t-v Print each command and its output. Default 'true'"
+   exit 1 # Exit script after printing help
+}
+
+verbose=false;
+alias='sat-dev';
+
+while getopts a:v opt 
+do
+   case "$opt" in
+   (a) alias="$OPTARG" ;;
+   (v) verbose=true ;;
+   (?) help ;;
+   esac
+done
+
+dx() { 
+    if [ "$verbose" = true ]
+    then
+      # Print command and run.
+      echo "\$ sfdx ${@/eval/}" ; sfdx "$@" ;
+      status=$?
+    else
+      # Suppress sfdx's output, and read the status property of the resulting JSON output instead. 
+      result=$(sfdx "$@" --json 2> /dev/null);
+      status=$(jq .status <<< $result)
+    fi
+    
+    # Exit early on error.
+    if [ $status -eq 1 ]
+    then 
+      if [ "$verbose" = false ]
+      then
+         echo " Error $(jq .message <<< $result) on \"$@\""
+      fi
+
+      exit 1;
+    fi
+}
+
+echo "Creating scratch org, \"$alias\"..."
+dx force:org:create -v devhub -a $alias -f config/project-scratch-def.json -d 7 -s
+
+echo "Uploading source code..."
+dx force:source:push -u $alias 
+
+echo "Assigning permissions..."
+dx force:user:permset:assign -u $alias -n SA_Administrator
+dx force:apex:execute -u $alias -f scripts/apex/set-current-user.apex
+
+echo "Uploading data..."
+dx force:data:bulk:upsert -u $alias -s Drug__c -f data/drugs.csv -i Drug_Code__c -w 5 
+dx force:data:bulk:upsert -u $alias -s Account -f data/accounts.csv -i Id -w 5 
+dx force:data:bulk:upsert -u $alias -s Case -f data/cases.csv -i Id -w 5
+dx force:data:tree:import -p data/comm-plan.json
+dx force:apex:execute -u $alias -f scripts/apex/add-comm-users.apex
+dx force:apex:execute -u $alias -f scripts/apex/assign-cases-to-ecs.apex
+dx force:apex:execute -u $alias -f scripts/apex/assign-cases-to-queue.apex
+dx force:apex:execute -u $alias -f scripts/apex/add-form-questions.apex
+dx force:data:bulk:upsert -u $alias -s Adjudication_Criteria__c -f data/adjudicationcriteria.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Diagnosis__c -f data/diagnosis.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Drug_Template__c -f data/drugtemplates.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Product_Health_Category__c -f data/producthealthcategories.csv -i Id -w 5
+dx force:apex:execute -u $alias -f scripts/apex/assign-drug-default-queues.apex
+
+echo "$alias is ready."
+dx force:org:open -u $alias;
