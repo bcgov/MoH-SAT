0b6b5f43b023a50491278be7782fed6fd68ce850
Jan Julian <jan@graycloud.ca>
2021-05-27 15:30:05 -0700

    ESA-493 UI changes. Scratch org data fixes.   (#468)
    * Replace custom title field with standard name. Add field help texts.
    
    * Remove reference to TItle__c
    
    * Fix pattern matching.
    
    * Add description to migrated records.
    
    * Move import sheets for adjudication criteria and drug template to step and actions.
    
    * Improve dev-setup script.

--- dev-setup.sh
+++ dev-setup.sh
@@ -2,18 +2,27 @@
 
 help() {
    echo ""
-   echo "Usage: $0 -a <string>"
-   echo -e "\t-a Set an alias for scratch org. Default 'sat-dev'."
+   echo "USAGE:"
+   echo "  $0 [-a <string>] [-d <integer>]"
+   echo ""
+   echo "OPTIONS:"
+   echo -e " -a Alias. Set a custom alias for scratch org. Default: 'sat-dev'."
+   echo -e " -d Duration. Set a custom duration for a scratch org. Default: 7, min: 1, max: 30."
+   echo ""
+   echo "EXAMPLES:"
+   echo -e "\t $ $0 -a feature-x -d 20"
    exit 1 # Exit script after printing help
 }
 
 verbose=true;
+duration=7;
 alias='sat-dev';
 
-while getopts a: opt 
+while getopts a:d: opt 
 do
    case "$opt" in
    (a) alias="$OPTARG" ;;
+   (d) duration="$OPTARG" ;;
    (?) help ;;
    esac
 done
@@ -44,7 +53,7 @@ dx() {
 }
 
 echo "Creating scratch org, \"$alias\"..."
-dx force:org:create -v devhub -a $alias -f config/project-scratch-def.json -d 7 -s
+dx force:org:create -v devhub -a $alias -f config/project-scratch-def.json -d $duration -s
 dx force:data:record:update -u $alias -s Organization -w "Name='Special Authority Scratch Org'" -v "TimeZoneSidKey='America/Los_Angeles'"
 dx force:data:record:update -u $alias -s User -w "Name='User User'" -v "TimeZoneSidKey='America/Los_Angeles'"
 
@@ -59,15 +68,16 @@ echo "Uploading data..."
 dx force:data:bulk:upsert -u $alias -s Drug__c -f data/drugs.csv -i Drug_Code__c -w 5 
 dx force:data:bulk:upsert -u $alias -s Account -f data/accounts.csv -i Id -w 5 
 dx force:data:bulk:upsert -u $alias -s Case -f data/cases.csv -i Id -w 5
-dx force:data:tree:import -p data/comm-plan.json
+dx force:data:tree:import -u $alias -p data/comm-plan.json
 dx force:apex:execute -u $alias -f scripts/apex/add-comm-users.apex
 dx force:apex:execute -u $alias -f scripts/apex/assign-cases-to-ecs.apex
 dx force:apex:execute -u $alias -f scripts/apex/assign-cases-to-queue.apex
 dx force:apex:execute -u $alias -f scripts/apex/add-form-questions.apex
-dx force:data:bulk:upsert -u $alias -s Adjudication_Criteria__c -f data/adjudicationcriteria.csv -i Id -w 5
 dx force:data:bulk:upsert -u $alias -s Diagnosis__c -f data/diagnosis.csv -i Id -w 5
-dx force:data:bulk:upsert -u $alias -s Drug_Template__c -f data/drugtemplates.csv -i Id -w 5
 dx force:data:bulk:upsert -u $alias -s Product_Health_Category__c -f data/producthealthcategories.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Step__c -f data/steps.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Step_Criteria__c -f data/stepcriteria.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Step_Action__c -f data/stepactions.csv -i Id -w 5
 dx force:apex:execute -u $alias -f scripts/apex/assign-drug-default-queues.apex
 
 echo "$alias is ready."
