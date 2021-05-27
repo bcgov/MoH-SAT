ee60dee9ccf301e8cf26b7c8016e66da9355efe8
Jan Julian <jan@graycloud.ca>
2021-05-27 13:53:10 -0700

    Move import sheets for adjudication criteria and drug template to step and actions.

--- dev-setup.sh
+++ dev-setup.sh
@@ -44,7 +44,7 @@ dx() {
 }
 
 echo "Creating scratch org, \"$alias\"..."
-dx force:org:create -v devhub -a $alias -f config/project-scratch-def.json -d 7 -s
+dx force:org:create -v devhub -a $alias -f config/project-scratch-def.json -d $duration -s
 dx force:data:record:update -u $alias -s Organization -w "Name='Special Authority Scratch Org'" -v "TimeZoneSidKey='America/Los_Angeles'"
 dx force:data:record:update -u $alias -s User -w "Name='User User'" -v "TimeZoneSidKey='America/Los_Angeles'"
 
@@ -59,15 +59,16 @@ echo "Uploading data..."
 dx force:data:bulk:upsert -u $alias -s Drug__c -f data/drugs.csv -i Drug_Code__c -w 5 
 dx force:data:bulk:upsert -u $alias -s Account -f data/accounts.csv -i Id -w 5 
 dx force:data:bulk:upsert -u $alias -s Case -f data/cases.csv -i Id -w 5
-dx force:data:tree:import -p data/comm-plan.json
+dx force:data:tree:import -u $alias -p data/comm-plan.json
 dx force:apex:execute -u $alias -f scripts/apex/add-comm-users.apex
 dx force:apex:execute -u $alias -f scripts/apex/assign-cases-to-ecs.apex
 dx force:apex:execute -u $alias -f scripts/apex/assign-cases-to-queue.apex
 dx force:apex:execute -u $alias -f scripts/apex/add-form-questions.apex
-dx force:data:bulk:upsert -u $alias -s Adjudication_Criteria__c -f data/adjudicationcriteria.csv -i Id -w 5
 dx force:data:bulk:upsert -u $alias -s Diagnosis__c -f data/diagnosis.csv -i Id -w 5
-dx force:data:bulk:upsert -u $alias -s Drug_Template__c -f data/drugtemplates.csv -i Id -w 5
 dx force:data:bulk:upsert -u $alias -s Product_Health_Category__c -f data/producthealthcategories.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Step__c -f data/steps.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Step_Criteria__c -f data/stepcriteria.csv -i Id -w 5
+dx force:data:bulk:upsert -u $alias -s Step_Action__c -f data/stepactions.csv -i Id -w 5
 dx force:apex:execute -u $alias -f scripts/apex/assign-drug-default-queues.apex
 
 echo "$alias is ready."
