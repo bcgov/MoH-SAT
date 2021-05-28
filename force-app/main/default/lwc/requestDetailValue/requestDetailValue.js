1c2dcf3c73272c9f145ec26a409dbfe82facd315
Jan Julian <jan@graycloud.ca>
2021-05-28 16:00:03 -0700

    ESA-1377 Editable request details for admins. (#470)

--- force-app/main/default/lwc/requestDetailValue/requestDetailValue.js
+++ force-app/main/default/lwc/requestDetailValue/requestDetailValue.js
@@ -1,32 +1,107 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import TIMEZONE from '@salesforce/i18n/timeZone';
-
-
+import OBJ_REQUEST_DETAIL from '@salesforce/schema/Request_Detail__c';
+import UserPreferencesShowFaxToGuestUsers from '@salesforce/schema/User.UserPreferencesShowFaxToGuestUsers';
 export default class RequestDetailValue extends LightningElement {
-    @api record;
     timezone = TIMEZONE;
+    
+    @wire(getObjectInfo, { objectApiName: OBJ_REQUEST_DETAIL })
+    rdObjInfo;
+
+    recordId;
+    type;
+    value;
+    
+    mode = 'view';
+
+    @api
+    set record(value) {
+        this.recordId = value.Id;
+
+        if (value.String_Value__c != null) {
+            this.type = 'string';
+            this.value = value.String_Value__c;
+        } else if (value.Datetime_Value__c != null) {
+            this.type = 'datetime';
+            this.value = value.Datetime_Value__c;
+        } else if (value.Date_Value__c != null) {
+            this.type = 'date';
+            this.value = value.Date_Value__c;
+        } else if (value.Decimal_Value__c != null) {
+            this.type = 'decimal';
+            this.value = value.Decimal_Value__c;
+        } else {
+            this.type = 'boolean';
+            this.value = value.Boolean_Value__c;
+        } 
+    }
+
+    get record() {
+        return this.value;
+    }
 
     get isString() {
-        return this.record.String_Value__c != null;
+        return this.type == 'string';
     }
 
     get isDatetime() {
-        return this.record.Datetime_Value__c != null;
+        return this.type == 'datetime';
     }
 
     get isDecimal() {
-        return this.record.Decimal_Value__c != null;
+        return this.type == 'decimal';
     }
 
     get isDate() {
-        return this.record.Date_Value__c != null;
+        return this.type == 'date';
     }
 
     get isBoolean() {
-        return !this.isString && 
-            !this.isDatetime && 
-            !this.isDate &&
-            !this.isDecimal && 
-            this.record.Boolean_Value__c !== undefined;
+        return this.type == 'boolean'
+    }
+
+    get editable() {
+        return this.rdObjInfo?.data?.updateable;
+    }
+
+    get isViewing() {
+        return this.mode == 'view';
+    }
+
+    get isEditing() {
+        return this.editable && this.mode == 'edit';
+    }
+
+    get showEdit() {
+        return this.editable && this.mode == 'view'
+    }
+
+    editHandler() {
+        if (!this.editable || this.mode == 'edit') return;
+
+        this.mode = 'edit';
+    }
+
+    submitHandler(event) {
+        event.preventDefault();
+        const fields = event.detail.fields;
+        this.template.querySelector('lightning-record-edit-form').submit(fields);
+    }
+
+    successHandler(event) {
+        this.record = {
+            Id: event.detail.id,
+            String_Value__c: event.detail.fields.String_Value__c.value,
+            Date_Value__c: event.detail.fields.Date_Value__c.value,
+            Datetime_Value__c: event.detail.fields.Datetime_Value__c.value,
+            Decimal_Value__c: event.detail.fields.Decimal_Value__c.value,
+            Boolean_Value__c: event.detail.fields.Boolean_Value__c.value
+        };
+        this.mode = 'view';
+    }
+
+    cancelHandler(event) {
+        this.mode = 'view';
     }
 }
