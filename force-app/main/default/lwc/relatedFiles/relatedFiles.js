959ae5759e9cf27e6b6c069e33da56c9505f49f6
Jan Julian <jan@graycloud.ca>
2021-12-07 12:58:00 -0800

    ESA-1767 Upload files. Minor refactor of relatedFiles component. (#602)

--- force-app/main/default/lwc/relatedFiles/relatedFiles.js
+++ force-app/main/default/lwc/relatedFiles/relatedFiles.js
@@ -1,33 +1,55 @@
 import { LightningElement, wire, api } from 'lwc';
 import getRelatedFiles from '@salesforce/apex/RelatedFiles.getRelatedFiles';
+import { refreshApex } from '@salesforce/apex'
 import { NavigationMixin } from 'lightning/navigation';
 
+const columns = [
+  { label: 'Name', fieldName: 'Title', type: 'text', hideDefaultActions: true, },
+  { label: "Date", fieldName: "createdDate", type: "date", fixedWidth: 165, hideDefaultActions: true, typeAttributes: { year: "numeric", month: "short", day: "2-digit", hour: "2-digit", minute: "2-digit" }},
+  { label: 'View', type: 'button', fixedWidth: 80, hideDefaultActions: true, typeAttributes: { label: 'View', name: 'view_file', title: 'View'}}
+];
 export default class RelatedFiles extends NavigationMixin(LightningElement) {
     @api recordId;
 
-    @api
-    hideQidToggle = false;
+    @api hideQidToggle = false;
 
     @wire(getRelatedFiles, { caseId: '$recordId'})
     records;
 
     showIds = false;
 
+    columns = columns;
+
     get hasRecords() {
       return this.records && this.records.data && this.records.data.length > 0;
     }
 
-    handleDownload(event) {
+    handleRowAction(event) {
+      const action = event.detail.action;
+      const row = event.detail.row;
+
+      switch (action.name) {
+        case 'view_file':
+          this.handleDownload(row.URL);
+          break;
+      }
+    }
+
+    handleDownload(url) {
       this[NavigationMixin.Navigate]({
         type: 'standard__webPage',
         attributes: {
-          url: event.target.value
+          url: url
         },
         state: {
-          recordIds: event.target.value,
-          selectedRecordId: event.target.value
+          recordIds: url,
+          selectedRecordId: url
         }
       });
+    }
+
+    handleUploadFinished(event) {
+      refreshApex(this.records);
     }
 
     toggleIds() {
