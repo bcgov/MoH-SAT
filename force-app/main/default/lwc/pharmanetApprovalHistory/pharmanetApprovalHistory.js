0e165f6f9cd9c2f0f3517ce94904a49e99d1851a
Jonathan Kim <35930820+youngbeanie@users.noreply.github.com>
2022-02-09 15:46:55 -0800

    ESA-1815 Add SA Approval Update/Terminate feature (#621)
    * SA Logs, Patient Page Layouts, Permission Set changes.
    
    * New classes for SAApprovalUpdate, changes to saHistoryLookup
    
    * Added update and terminate functionality, logging events
    
    * Updated fetch integration logs, deleted SA Logs obj, added test classes
    
    * ESA-1815 removed SA_Log references in Perm Sets

--- force-app/main/default/lwc/pharmanetApprovalHistory/pharmanetApprovalHistory.js
+++ force-app/main/default/lwc/pharmanetApprovalHistory/pharmanetApprovalHistory.js
@@ -1,5 +1,7 @@
 import { LightningElement, api } from 'lwc';
 import fetchSAApprovalHistoryByCase from '@salesforce/apex/ODRIntegration.fetchSAApprovalHistoryByCase';
+import fetchIntegrationLogs from '@salesforce/apex/ODRIntegration.fetchIntegrationLogs';
+import getPatientIdentifier from '@salesforce/apex/ODRIntegration.getPatientIdentifier';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const columns = [
@@ -14,13 +16,16 @@ const columns = [
   { label: 'Excluded Plans', fieldName: 'excludedPlans', type: 'text', wrapText: true, hideDefaultActions: true },
   { label: 'Pharmacy', fieldName: 'pharmacyID', type: 'text', wrapText: true, hideDefaultActions: true },
   { label: 'DEC', fieldName: 'decCode', type: 'text', wrapText: true, hideDefaultActions: true },
-  { label: 'CreatedBy', fieldName: 'createdBy', type: 'text', wrapText: true, hideDefaultActions: true }
+  { label: 'CreatedBy', fieldName: 'createdBy', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Log', fieldName: 'integrationLog', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Terminate', type: 'button', typeAttributes: { label: 'Terminate', name: 'terminate'} }
 ];
 
 export default class PharmanetApprovalHistory extends LightningElement {
   @api recordId;
   columns = columns;
   data = [];
+  patientIdentifier;
   loaded = false;
   hasResults = false;
   completeAndNoResults = false;
@@ -28,8 +33,38 @@ export default class PharmanetApprovalHistory extends LightningElement {
   error = {};
   isError = false;
 
+  openModal = false;
+  selectedSARecord;
+  saApprovalRequestFormatData = [];
+
   connectedCallback() {
     this.fetchItems();
+  }
+
+  handleRowAction(event) {
+    const actionName = event.detail.action.name;
+    const row = event.detail.row;
+    switch (actionName) {
+      case 'terminate':
+        console.log(JSON.stringify(row));
+        this.openUpdateModal(row.index);
+        break;
+      default:
+    }
+  }
+
+  openUpdateModal(index){
+    this.saApprovalRequestFormatData.forEach((record) => { 
+      if(record.index == index){
+        this.selectedSARecord = record;
+      }
+    });
+    this.openModal = true;
+  }
+
+  closeUpdateModal(){
+    this.fetchItems();
+    this.openModal = false;
   }
 
   convertSAType(type) {
@@ -56,8 +91,39 @@ export default class PharmanetApprovalHistory extends LightningElement {
     }
   }
 
+  generateSingleKey(record){
+    return this.patientIdentifier + (record.specialItem.din || "null") + (record.specialItem.rdp || "null") + record.specAuthType + record.effectiveDate.replace(/-/g,"");
+  }
+
+  generateKeys(saRecords){
+    let keys = [];
+    saRecords.forEach(record => {
+      let key = this.generateSingleKey(record);
+      keys.push(key);
+    })
+    return keys;
+  }
+
+  getLatestLog(key, logs){
+    if (logs[key] === undefined) return;
+    let log = logs[key];
+    let logMessage = '';
+
+    if (log.Code__c != 200 || log.Code__c != 201){
+      logMessage = log.Type__c == 'SA Approval Update Request' ? 'Failed update ' : 'Failed termination ';  
+    } else {
+      logMessage = log.Type__c == 'SA Approval Update Request' ? 'Updated on ' : 'Terminated on ';
+    }
+    logMessage += log.Timestamp__c.slice(0,10);
+
+    return logMessage;
+  }
+
   async fetchItems() {
-    let data = await fetchSAApprovalHistoryByCase({recordId: this.recordId})
+    let data = await fetchSAApprovalHistoryByCase({recordId: this.recordId});
+    this.patientIdentifier = await getPatientIdentifier({recordId: this.recordId});
+    let keys = this.generateKeys(data.saRecords);
+    let logs = await fetchIntegrationLogs({phn: this.patientIdentifier, keys: keys});
     if (data && data.error == null) {
       const records = data.saRecords;
       this.totalRecords = data.totalRecords;
@@ -66,7 +132,23 @@ export default class PharmanetApprovalHistory extends LightningElement {
         this.completeAndNoResults = false;
         this.hasResults = true;
         let dataArray = [];
+        let index = 0;
+
         records.forEach(record => {
+          // Needed because SAApprovalHistoryResponse is a different format than SAApprovalRequest.
+          let saRecord = {saRecord: {
+            phn: this.patientIdentifier,
+            saRequester: record.saRequester,
+            specialItem: record.specialItem,
+            specAuthType: record.specAuthType,
+            justificationCodes: record.justificationCodes,
+            excludedPlans: record.excludedPlans,
+            effectiveDate: record.effectiveDate,
+            terminationDate: record.terminationDate,
+            maxDaysSupply: record.maxDaysSupply,
+            maxPricePct: record.maxPricePct
+          }};
+
           let item = {};
           item['description'] = record.specialItem.itemDescription;
           item['dinrdp'] = this.convertDINPIN(record.specAuthType, record.specialItem.din || record.specialItem.rdp);
@@ -89,7 +171,11 @@ export default class PharmanetApprovalHistory extends LightningElement {
           // Not coming in response.
           item['decCode'] = record.saRequester.decCode;
           item['createdBy'] = record.createdBy;
+          item['integrationLog'] = this.getLatestLog(this.generateSingleKey(record), logs);
+          item['index'] = index;
+          saRecord.index = index++;
           dataArray.push(item);
+          this.saApprovalRequestFormatData.push(saRecord);        
         });
         this.data = dataArray;
       } else {
--- force-app/main/default/lwc/saHistoryLookup/saHistoryLookup.js
+++ force-app/main/default/lwc/saHistoryLookup/saHistoryLookup.js
@@ -1,5 +1,6 @@
 import { LightningElement } from 'lwc';
 import fetchSAApprovalHistory from '@salesforce/apex/ODRIntegration.fetchSAApprovalHistory';
+import fetchIntegrationLogs from '@salesforce/apex/ODRIntegration.fetchIntegrationLogs';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const columns = [
@@ -7,7 +8,9 @@ const columns = [
   { label: 'RDP or DIN/PIN', fieldName: 'dinrdp', type: 'text', wrapText: true, hideDefaultActions: true },
   { label: 'Effective Date', fieldName: 'effectiveDate', wrapText: true, type: 'date-local', typeAttributes:{ month: "2-digit", day: "2-digit" }, hideDefaultActions: true },
   { label: 'Termination Date', fieldName: 'terminationDate', wrapText: true, type: 'date-local', typeAttributes:{ month: "2-digit", day: "2-digit" }, hideDefaultActions: true },
-  { label: 'Auth Type', fieldName: 'specAuthType', type: 'text', wrapText: true, hideDefaultActions: true }
+  { label: 'Auth Type', fieldName: 'specAuthType', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Log', fieldName: 'integrationLog', type: 'text', wrapText: true, hideDefaultActions: true },
+  { label: 'Update', type: 'button', typeAttributes: { label: 'Update', name: 'update'} }
 ];
 
 export default class SaHistoryLookup extends LightningElement {
@@ -24,6 +27,10 @@ export default class SaHistoryLookup extends LightningElement {
   error = {};
   isError = false;
 
+  openModal = false;
+  selectedSARecord;
+  saApprovalRequestFormatData = [];
+
   get patientId() {
     return this.template.querySelector('.patientIdentifier').value;
   }
@@ -31,6 +38,31 @@ export default class SaHistoryLookup extends LightningElement {
   handleFormChange(event) {
     this.patientIdentifier = event.target.value;
     this.template.querySelector('.btn-lookup').disabled = false;
+  }
+
+  handleRowAction(event) {
+    const actionName = event.detail.action.name;
+    const row = event.detail.row;
+    switch (actionName) {
+      case 'update':
+        this.openUpdateModal(row.index);
+        break;
+      default:
+    }
+  }
+
+  openUpdateModal(index){
+      this.saApprovalRequestFormatData.forEach((record) => { 
+        if(record.index == index){
+          this.selectedSARecord = record;
+        }
+      });
+      this.openModal = true;
+  }
+
+  closeUpdateModal(){
+    this.fetchItems();
+    this.openModal = false;
   }
 
   async handleLookup() {
@@ -74,10 +106,40 @@ export default class SaHistoryLookup extends LightningElement {
     this.fetchItems();
   }
 
+  generateSingleKey(record){
+    return this.patientIdentifier + (record.specialItem.din || "null") + (record.specialItem.rdp || "null") + record.specAuthType + record.effectiveDate.replace(/-/g,"");
+  }
+
+  generateKeys(saRecords){
+    let keys = [];
+    saRecords.forEach(record => {
+      let key = this.generateSingleKey(record);
+      keys.push(key);
+    })
+    return keys;
+  }
+
+  getLatestLog(key, logs){
+    if (logs[key] === undefined) return;
+    let log = logs[key];
+    let logMessage = '';
+
+    if (log.Code__c != 200 || log.Code__c != 201){
+      logMessage = log.Type__c == 'SA Approval Update Request' ? 'Failed update ' : 'Failed termination ';  
+    } else {
+      logMessage = log.Type__c == 'SA Approval Update Request' ? 'Updated on ' : 'Terminated on ';
+    }
+    logMessage += log.Timestamp__c.slice(0,10);
+
+    return logMessage;
+  }
+
   async fetchItems() {
     if (this.patientIdentifier == null || this.patientIdentifier.length < 1) return;
 
     let data = await fetchSAApprovalHistory({phn: this.patientIdentifier})
+    let keys = this.generateKeys(data.saRecords);
+    let logs = await fetchIntegrationLogs({phn: this.patientIdentifier, keys: keys});
     if (data && data.error == null) {
       const records = data.saRecords;
       this.totalRecords = data.totalRecords;
@@ -86,8 +148,23 @@ export default class SaHistoryLookup extends LightningElement {
         this.completeAndNoResults = false;
         this.hasResults = true;
         let dataArray = [];
+        let index = 0;
         
         records.forEach(record => {
+          // Needed because SAApprovalHistoryResponse is a different format than SAApprovalRequest (pnetSaForm).
+          let saRecord = {saRecord: {
+            phn: this.patientIdentifier,
+            saRequester: record.saRequester,
+            specialItem: record.specialItem,
+            specAuthType: record.specAuthType,
+            justificationCodes: record.justificationCodes,
+            excludedPlans: record.excludedPlans,
+            effectiveDate: record.effectiveDate,
+            terminationDate: record.terminationDate,
+            maxDaysSupply: record.maxDaysSupply,
+            maxPricePct: record.maxPricePct
+          }};
+
           // Is there a filter applied?
           if (this.dinRdpFilter.length == 0
               || record.specialItem.din?.indexOf(this.dinRdpFilter.trim()) > -1
@@ -104,7 +181,11 @@ export default class SaHistoryLookup extends LightningElement {
               item['specAuthType'] = this.convertSAType(record.specAuthType);
               item['effectiveDate'] = record.effectiveDate;
               item['terminationDate'] = record.terminationDate;
+              item['integrationLog'] = this.getLatestLog(this.generateSingleKey(record), logs);
+              item['index'] = index;
+              saRecord.index = index++;
               dataArray.push(item);
+              this.saApprovalRequestFormatData.push(saRecord);
             }
           }
         });
