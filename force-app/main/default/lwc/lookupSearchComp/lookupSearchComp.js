cd27792f87ee0de357629cabf858b3173a8d77f6
sumancdey <suman.c.dey@accenture.com>
2024-01-12 02:37:32 +0530

    Flow Defect fix for Patient & Provider Search

--- force-app/main/default/lwc/lookupSearchComp/lookupSearchComp.js
+++ force-app/main/default/lwc/lookupSearchComp/lookupSearchComp.js
@@ -23,59 +23,72 @@ export default class CustomObjectForm extends LightningElement {
 @api PostalCode;
 @api availableActions = [];
 handlekeychange(event) {
-        this.accountPHN = event.currentTarget.value; 
-        }
-handleSearch() {
-        if(!this.accountPHN) {
-            this.messageResult = true;
-            this.accountList = undefined;
-            return;
-        }
-        getAccount({actPHN : this.accountPHN})
-        .then(result => {
-            this.accountList = result;
-            this.Birthdate = result[0].PersonContact.Birthdate;
-            this.Name = result[0].Name;
-            this.AccountId = result[0].Id;
-            this.showRemoveButton=true;
-            this.messageResult = false;
-            this.resultLength=result.length;
-        })
-        .catch(error => {
-            this.accountList = undefined;
-            this.messageResult = true;
-            if(error) {
-                if (Array.isArray(error.body)) {
-                    this.errorMsg = error.body.map(e => e.message).join(', ');
-                } else if (typeof error.body.message === 'string') {
-                    this.errorMsg = error.body.message;
-                }
-            }
-        }) 
+    this.accountPHN = event.currentTarget.value; 
     }
-    handleRemoveResults(){
+handleSearch() {
+    if(!this.accountPHN) {
+        this.messageResult = true;
+        this.accountList = undefined;
+        return;
+    }
+    getAccount({actPHN : this.accountPHN})
+    .then(result => {
+        this.accountList = result;
+        this.Birthdate = result[0].PersonContact.Birthdate;
+        this.Name = result[0].Name;
+        this.AccountId = result[0].Id;
+        this.showRemoveButton=true;
+        this.messageResult = false;
+        this.resultLength=result.length;
+    })
+    .catch(error => {
+        this.accountList = undefined;
+        this.messageResult = true;
+        if(error) {
+            if (Array.isArray(error.body)) {
+                this.errorMsg = error.body.map(e => e.message).join(', ');
+            } else if (typeof error.body.message === 'string') {
+                this.errorMsg = error.body.message;
+            }
+        }
+    }) 
+}
+handleRemoveResults(){
+    this.resetForm();
+    this.showRemoveButton = false;
+}
+handleNext(){
+    if (this.shouldShowErrorMessage()) {
+        this.handleNoSearchResult();
+    } else if (this.shouldDispatchNextEvent()) {
+        const navigateNextEvent = new FlowNavigationNextEvent();
+        this.dispatchEvent(navigateNextEvent);
+    }
+}
+handleBack(){
+    if (this.availableActions.includes('BACK')) {
+        const navigateBackEvent = new FlowNavigationBackEvent();
+        this.dispatchEvent(navigateBackEvent);
+    }
+}
+// Helper methods
+handleNoSearchResult() {
+    this.accountList = [];
+    this.messageResult = true;
+}
+resetForm() {
     this.accountList = [];
     this.Birthdate = '';
     this.Name = '';
     this.AccountId = '';
     this.accountPHN = '';
-    this.showRemoveButton=false;
     this.messageResult = false;
-    this.resultLength=0;
-    }
-    handleNext(){
-    if (this.resultLength === 0 || this.resultLength === undefined ||!this.accountPHN) {
-        this.messageResult = true;
-        this.accountList = undefined;
-    }else if (this.availableActions.find((action) => action === "NEXT")){
-            const navigateNextEvent = new FlowNavigationNextEvent();
-            this.dispatchEvent(navigateNextEvent);
-            }
-    }
-    handleBack(){
-        if (this.availableActions.find((action) => action === "BACK")){
-            const navigateBackEvent = new FlowNavigationBackEvent();
-            this.dispatchEvent(navigateBackEvent);
-            }
-    }
+    this.resultLength = 0;
+}
+shouldShowErrorMessage() {
+    return this.resultLength === 0 || this.resultLength === undefined || !this.accountPHN;
+}
+shouldDispatchNextEvent() {
+    return this.availableActions.includes('NEXT');
+}
 }
--- force-app/main/default/lwc/providerAccountComp/providerAccountComp.js
+++ force-app/main/default/lwc/providerAccountComp/providerAccountComp.js
@@ -1,77 +1,91 @@
 import { LightningElement, api } from 'lwc';
 import {
-    FlowNavigationBackEvent,
-    FlowNavigationNextEvent
-  } from "lightning/flowSupport";
+FlowNavigationBackEvent,
+FlowNavigationNextEvent
+} from "lightning/flowSupport";
 import getProviderAccount from '@salesforce/apex/EDRDAccountLookupController.getProviderAccount';
 export default class ProviderAccountComp extends LightningElement {
-    @api accountPHN = '';
-    @api providerPHN;
-    @api accountName;
-    @api messageResult=false;
-    @api showsearchedvalues=false;
-    @api showRemoveButton=false;
-    @api accountList = [];
-    @api Type;
-    @api Name;
-    @api ProviderAccId;
-    @api ProviderIdentifier;
-    @api availableActions = [];
-    handlekeychange(event) {
-            this.accountPHN = event.currentTarget.value; 
-           }
-    handleSearch() {
-            if(!this.accountPHN) {
-                this.messageResult=true;
-                this.accountList = undefined;
-                return;
-            }
-            getProviderAccount({providerAct : this.accountPHN})
-            .then(result => {
-                this.accountList = result;
-                this.Name = result[0].Name;
-                this.ProviderAccId = result[0].Id;
-                this.ProviderIdentifier = result[0].Patient_Identifier__pc;
-                this.Type = result[0].Provider_Type__pc;
-                this.showRemoveButton=true;
-                this.messageResult=false;
-                this.resultLength=result.length;
-            })
-            .catch(error => {
-                this.accountList = undefined;
-                this.messageResult = true;
-                if(error) {
-                    if (Array.isArray(error.body)) {
-                        this.errorMsg = error.body.map(e => e.message).join(', ');
-                    } else if (typeof error.body.message === 'string') {
-                        this.errorMsg = error.body.message;
-                    }
-                }
-            }) 
+@api accountPHN = '';
+@api providerPHN;
+@api accountName;
+@api messageResult=false;
+@api showsearchedvalues=false;
+@api showRemoveButton=false;
+@api accountList = [];
+@api Type;
+@api Name;
+@api ProviderAccId;
+@api ProviderIdentifier;
+@api availableActions = [];
+handlekeychange(event) {
+        this.accountPHN = event.currentTarget.value; 
         }
-        handleRemoveResults(){
-            this.accountList = [];
-            this.Name = '';
-            this.ProviderAccId = '';
-            this.Type = '';
-            this.providerPHN = '';
-            this.showRemoveButton=false;
+handleSearch() {
+        if(!this.accountPHN) {
+            this.messageResult=true;
+            this.accountList = undefined;
+            return;
+        }
+        getProviderAccount({providerAct : this.accountPHN})
+        .then(result => {
+            this.accountList = result;
+            this.Name = result[0].Name;
+            this.ProviderAccId = result[0].Id;
+            this.ProviderIdentifier = result[0].Patient_Identifier__pc;
+            this.Type = result[0].Provider_Type__pc;
+            this.showRemoveButton=true;
             this.messageResult=false;
-            this.resultLength=0;
-            }
-        handleNext(){
-        if (this.resultLength === 0 || this.resultLength === undefined ||!this.accountPHN){
-          this.messageResult = true;
-          this.accountList = undefined;
-        }else if (this.availableActions.find((action) => action === "NEXT")){
-               const navigateNextEvent = new FlowNavigationNextEvent();
-               this.dispatchEvent(navigateNextEvent);
-               }
-        }
-        handleBack(){
-            if (this.availableActions.find((action) => action === "BACK")){
-                const navigateBackEvent = new FlowNavigationBackEvent();
-                this.dispatchEvent(navigateBackEvent);
+            this.resultLength=result.length;
+        })
+        .catch(error => {
+            this.accountList = undefined;
+            this.messageResult = true;
+            if(error) {
+                if (Array.isArray(error.body)) {
+                    this.errorMsg = error.body.map(e => e.message).join(', ');
+                } else if (typeof error.body.message === 'string') {
+                    this.errorMsg = error.body.message;
                 }
-        }
+            }
+        }) 
     }
+    handleRemoveResults(){
+        this.resetForm();
+    this.showRemoveButton = false;
+}
+handleNext(){
+    if (this.shouldShowErrorMessage()) {
+        this.handleNoSearchResult();
+    } else if (this.shouldDispatchNextEvent()) {
+        const navigateNextEvent = new FlowNavigationNextEvent();
+        this.dispatchEvent(navigateNextEvent);
+    }
+}
+handleBack(){
+    if (this.availableActions.includes('BACK')) {
+        const navigateBackEvent = new FlowNavigationBackEvent();
+        this.dispatchEvent(navigateBackEvent);
+    }
+}
+    // Helper methods
+handleNoSearchResult() {
+    this.accountList = [];
+    this.messageResult = true;
+}
+resetForm() {
+    this.accountList = [];
+    this.Name = '';
+    this.ProviderAccId = '';
+    this.Type = '';
+    this.providerPHN = '';
+    this.showRemoveButton=false;
+    this.messageResult=false;
+    this.resultLength=0;
+}
+shouldShowErrorMessage() {
+    return this.resultLength === 0 || this.resultLength === undefined || !this.accountPHN;
+}
+shouldDispatchNextEvent() {
+    return this.availableActions.includes('NEXT');
+}
+}
