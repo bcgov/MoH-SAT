ec0e6466bc19315b034a1403c4f38353d7126a09
deepakmulamalla <deepak.mulamalla@gov.bc.ca>
2025-06-02 16:12:23 -0400

    Changes related to EDRD-1484

--- force-app/main/default/lwc/eDRD_lwc_RelatedPhysicians/eDRD_lwc_RelatedPhysicians.js
+++ force-app/main/default/lwc/eDRD_lwc_RelatedPhysicians/eDRD_lwc_RelatedPhysicians.js
@@ -7,8 +7,7 @@ const columns = [
     { label: 'Serial Number', fieldName: 'serialNumber', type: 'number', initialWidth: 100, sortable: true },
     { label: 'Name', fieldName: 'Name', type: 'text', sortable: true },
     { label: 'Provider Type', fieldName: 'ProviderType', type: 'text', sortable: true },
-    { label: 'Provider Identifier', fieldName: 'ProviderIdentifier', type: 'text', sortable: true },
-    { label: 'Role', fieldName: 'Role', type: 'text', sortable: true }
+    { label: 'Provider Identifier', fieldName: 'ProviderIdentifier', type: 'text', sortable: true }
 ];
 
 export default class EdrdLwcRelatedPhysicians extends LightningElement {
